680c4253f95f0783e05aa2480fd69cfbd86e6786
Lisa L. Lowe <lllowe@ncsu.edu>
2022-07-22 09:54:45 -0400

    Layers for Stations

--- flask_oybcst.py
+++ flask_oybcst.py
@@ -9,9 +9,19 @@ app.testing = True
 
 @app.route("/")
 def hello():
-    #return "<h1 style='color:red'>Hello There!</h1>"
+
+# Initialize map
+    m = folium.Map(location=[30.50355, -87.98733], zoom_start=10)
+
+#ReadData and Define vars
     station_file = "RESTORE-Station-List-2020.csv"
     station_data = pd.read_csv(station_file)
+    water_group = folium.FeatureGroup(name="Water Stations")
+    fish_group = folium.FeatureGroup(name="Fisheries Stations")
+    both_group = folium.FeatureGroup(name="Water and Fisheries Stations")
+    none_group = folium.FeatureGroup(name="Stations",show=False)
+
+# locations and colors
     locations = station_data[['Lat', 'Lon']]
     locationlist = locations.values.tolist()
     Colors = ["white"]*len(locationlist)
@@ -21,6 +31,8 @@ def hello():
             Colors[point]=("blue")
         elif type(station_data["Fishery Station Names"][point]) != float:
             Colors[point]=("lightgreen")
+        else:
+            Colors[point]=("white")
         if type(station_data["Site Landmarks"][point]) != float:
             Icons[point]=("institution")
         if station_data["Water Station 2020 Restore"][point] == 'x' and type(station_data["Fishery Station Names"][point]) != float:
@@ -29,20 +41,27 @@ def hello():
     fish = station_data["Fishery Station Names"].fillna("")
     Landmarks = station_data["Site Landmarks"].fillna("")
     m = folium.Map(location=[30.50355, -87.98733], zoom_start=10)
+
+
 #    marker_cluster = MarkerCluster().add_to(m)
     folium.TileLayer('openstreetmap').add_to(m)
     folium.TileLayer('Stamen Terrain').add_to(m)
-    station_group = folium.FeatureGroup(name="Stations")
     for point in range(0, len(locationlist)):
-        station_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
-    m.add_child(station_group)
+        if Colors[point] == "blue": 
+            water_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
+        elif Colors[point] == "lightgreen":
+            fish_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
+        elif Colors[point] == "darkgreen":
+            both_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
+        else: 
+            none_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
+
+    m.add_child(water_group)
+    m.add_child(fish_group)
+    m.add_child(both_group)
+    m.add_child(none_group)
     folium.LayerControl().add_to(m)
 
-#    for point in range(0, len(locationlist)):
-#        folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')).add_to(m)
-#    for point in range(0, len(locationlist)):
-#        info = station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point] + ",icon=folium.Icon(color=" + Colors[point] + r",icon_color='red', icon=" + Icons[point] +", angle=0, prefix='fa')"
-#        folium.RegularPolygonMarker(locationlist[point], popup=info).add_to(marker_cluster)
 
     m.save('templates/map.html')
     return render_template('index.html') 
